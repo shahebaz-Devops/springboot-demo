33f4795602e216812b8286880c1208a11c46c658
shahebaz-Devops <shahebaz.shaikh9481@gmail.com>
2025-02-21 17:45:38 +0530

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sen-dev34343" # Replace with your actual S3 bucket name
+    bucket = "shahebaz-shaikh" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
